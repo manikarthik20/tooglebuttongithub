1e55bbafcf1431ae3d09d1f7697dda733dbe22c5
Manikarthik Chandankeri <mc21407@corp.ojas-it.com>
2022-08-10 17:31:45 +0530

    tooglebuttongithub

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import ToggleButton from './components/ToggleButton'
+
+const Checked = () => <>🤪</>;
+const UnChecked = () => <>🙂</>;
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>Toggle Button</p>
+        <ToggleButton />
+        <br/>
+        <p>Disabled Toggle Button</p>
+        <ToggleButton onChange={state => console.log(state)} disabled={true} />
+        <br/>
+        <p>Toggle Button - Default Value TRUE</p>
+        <ToggleButton onChange={state => console.log(state)} defaultChecked={true} />
+        <br/>
+        <p>Toggle Button - Icons Change</p>
+        <ToggleButton onChange={state => console.log(state)} icons={{checked: <Checked />, unchecked: <UnChecked />}} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
